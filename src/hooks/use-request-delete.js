c258ac5ced1bbc8f6fa9b2b6e9839862464185d9
Dmitry <dsilichev@gmail.com>
2024-04-15 17:36:52 +0300

    firebase build

--- src/hooks/use-request-delete.js
+++ src/hooks/use-request-delete.js
@@ -1,20 +1,32 @@
 import { useState } from 'react';
+import { ref, remove } from 'firebase/database';
+import { db } from '../firebase';
 
-export const useRequestDelete = (refreshProducts) => {
+export const useRequestDelete = () => {
   const [isDeleting, setIsDeleting] = useState(false);
 
   const requestDelete = () => {
     setIsDeleting(true);
-    fetch('http://localhost:3005/products/003', {
-      method: 'DELETE',
-    })
-      .then((rawResponse) => rawResponse.json())
+
+    const hairDryerDb = ref(db, 'products/003');
+
+    remove(hairDryerDb)
       .then((response) => {
-        refreshProducts();
         console.log('Фен удален', response);
       })
       .finally(() => setIsDeleting(false));
   };
+
+  //   fetch('http://localhost:3005/products/003', {
+  //     method: 'DELETE',
+  //   })
+  //     .then((rawResponse) => rawResponse.json())
+  //     .then((response) => {
+  //       refreshProducts();
+  //       console.log('Фен удален', response);
+  //     })
+  //     .finally(() => setIsDeleting(false));
+  // };
 
   return {
     isDeleting,
--- src/hooks/use-request-update.js
+++ src/hooks/use-request-update.js
@@ -1,25 +1,41 @@
 import { useState } from 'react';
+import { ref, set } from 'firebase/database';
+import { db } from '../firebase';
 
 export const useRequestUpdate = (refreshProducts) => {
   const [isUpdating, setIsUpdating] = useState(false);
 
   const requestUpdate = () => {
     setIsUpdating(true);
-    fetch('http://localhost:3005/products/002', {
-      method: 'PUT',
-      headers: { 'Content-Type': 'application/json;charset=utf-8' },
-      body: JSON.stringify({
-        name: 'Новый смартфон',
-        price: 30,
-      }),
+
+    const smartphoneDbRef = ref(db, 'products/002');
+
+    set(smartphoneDbRef, {
+      name: 'Смартфон',
+      price: 16700,
     })
-      .then((rawResponse) => rawResponse.json())
-      .then((response) => {
-        refreshProducts();
-        console.log(response);
-      })
-      .finally(() => setIsUpdating(false));
-  };
+    .then((response) => {
+      // refreshProducts();
+      console.log('Смартфон обновлен', response);
+    })
+    .finally(() => setIsUpdating(false));
+};
+
+  //   fetch('http://localhost:3005/products/002', {
+  //     method: 'PUT',
+  //     headers: { 'Content-Type': 'application/json;charset=utf-8' },
+  //     body: JSON.stringify({
+  //       name: 'Новый смартфон',
+  //       price: 30,
+  //     }),
+  //   })
+  //     .then((rawResponse) => rawResponse.json())
+  //     .then((response) => {
+  //       refreshProducts();
+  //       console.log(response);
+  //     })
+  //     .finally(() => setIsUpdating(false));
+  // };
   return {
     isUpdating,
     requestUpdate,
